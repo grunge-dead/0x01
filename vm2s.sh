030623a7e21b6111bff853d3e3b5be9ee99c6ef9
grunge-dead <66629738+grunge-dead@users.noreply.github.com>
2020-06-08 22:25:26 +0800

    Update vm2s.sh

--- vm2s.sh
+++ vm2s.sh
@@ -1,10 +1,11 @@
 cd &&
 mkdir fjiownc &&
 cd fjiownc &&
-cp /media/$(`getent passwd 1000 | cut -d ':' -f 1`)/"VMware Tools"/*.gz ./a.tar.gz &&
+cp /media/$(getent passwd 1000 | cut -d ':' -f 1)/"VMware Tools"/*.gz ./a.tar.gz &&
 tar -zxf *.gz &&
 cd vmware-tools-distrib &&
 sudo ./vmware-install.pl --f --d &&
 cd &&
 rm -rf fjiownc &&
 sudo reboot
+
