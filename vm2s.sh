44ef0043508c525a4134c4c7cb7ea945d0be8df7
grunge-dead <66629738+grunge-dead@users.noreply.github.com>
2020-06-08 22:14:43 +0800

    Create vm2s.sh

---
+++ vm2s.sh
@@ -0,0 +1,10 @@
+cd &&
+mkdir fjiownc &&
+cd fjiownc &&
+cp /media/$(`getent passwd 1000 | cut -d ':' -f 1`)/"VMware Tools"/*.gz ./a.tar.gz &&
+tar -zxf *.gz &&
+cd vmware-tools-distrib &&
+sudo ./vmware-install.pl --f --d &&
+cd &&
+rm -rf fjiownc &&
+sudo reboot
